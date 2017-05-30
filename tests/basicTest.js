63b3eca80068adea91eb9cc2cd1dc0a3943313aa
Cole Heath <coleheath@Tais-MacBook-Pro.local>
2017-05-30 16:29:00 -0500

    initial commit

---
+++ tests/basicTest.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'basicTest' : function (browser) {
+    browser
+      .url('https://the-internet.herokuapp.com/login')
+      .pause(1000)
+      .setValue('input[type=text]',"tomsmith")
+      .setValue('input[type=password]',"SuperSecretPassword!")
+      .click('button[type=submit]')
+      .pause(1000)
+      .assert.containsText('h2','Secure Area')
+      .end();
+  }
+};
